9f87a969a3c314632d7fa8720ffd81732aedb800
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-02-26 17:39:24 +0100

    Add post lane endpoint; check with Postman and working

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,5 +3,7 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
+// add a new lane
+router.route('/lanes').post(LaneController.addLane);
 
 export default router;
