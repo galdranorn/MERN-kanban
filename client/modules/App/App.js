9a3a2de441dbdf933078b02d50b0810da626fb3d
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-03-25 20:11:36 +0100

    Add some basic styles

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -7,12 +7,9 @@ import styles from './App.css';
 
 // Import Components
 import Helmet from 'react-helmet';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 
 // Import Actions
 import { toggleAddPost } from './AppActions';
-import { switchLanguage } from '../../modules/Intl/IntlActions';
 
 let DevTools;
 if (process.env.NODE_ENV === 'development') {
@@ -40,8 +37,7 @@ export class App extends Component {
         {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
         <div>
           <Helmet
-            title="MERN Starter - Blog App"
-            titleTemplate="%s - Blog App"
+            title="MERN kanban APP"
             meta={[
               { charset: 'utf-8' },
               {
@@ -54,15 +50,9 @@ export class App extends Component {
               },
             ]}
           />
-          <Header
-            switchLanguage={lang => this.props.dispatch(switchLanguage(lang))}
-            intl={this.props.intl}
-            toggleAddPost={this.toggleAddPostSection}
-          />
           <div className={styles.container}>
             {this.props.children}
           </div>
-          <Footer />
         </div>
       </div>
     );
